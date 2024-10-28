37a76105ed8310bc1427a79e82505d030624a742
Pascal Pieters <pascalaldo@gmail.com>
2024-10-28 16:40:46 +0100

    Complete iModulonDB datastructure

--- pankb_data_prep/imodulon/imodulon_data.py
+++ pankb_data_prep/imodulon/imodulon_data.py
@@ -1,4 +1,17 @@
 IMODULON_DATA = {
+    "Acinetobacter_baumannii": {
+        "GCF_000963815.1": ("a_baumannii",
+        [
+            ("precise139", "AbaumPRECISE139"),
+        ])
+    },
+    "Bacillus_subtilis": {
+        "GCF_964017255.1": ("b_subtilis",
+        [
+            ("nicolas", "Microarray"),
+            ("modulome", "Modulome"),
+        ])
+    },
     "Escherichia_coli": {
         "GCF_000005845.2": ("e_coli", 
         [
@@ -8,6 +21,67 @@ IMODULON_DATA = {
             ("precise1", "PRECISE-278 (1.0)"),
         ])
     },
+    "Limosilactobacillus_reuteri": {
+        "GCF_020785475.1": ("l_reuteri",
+        [
+            ("lactoprecise", "lactoPRECISE"),
+        ])
+    },
+    "Mycobacterium_tuberculosis": {
+        "GCF_000195955.2": ("m_tuberculosis",
+        [
+            ("modulome", "Modulome"),
+        ])
+    },
+    "Pseudomonas_E_putida": {
+        "GCF_000007565.2": ("p_putida", 
+        [
+            ("precise321", "putidaPRECISE321"),
+        ])
+    },
+    "Pseudomonas_syringae": {
+        "GCF_000007805.1": ("p_syringae",
+        [
+            ("precise202", "syringaePRECISE202"),
+        ])
+    },
+    "Salmonella_enterica": {
+        "GCF_000006945.2": ("s_enterica",
+        [
+            ("core", "Modulome Core"),
+        ])
+    },
+    "Sulfolobus_acidocaldarius": {
+        "GCF_000012285.1": ("s_acidocaldarius",
+        [
+            ("modulome", "Modulome"),
+        ])
+    },
+    "Staphylococcus_aureus": {
+        "GCF_000017085.1": ("s_aureus",
+        [
+            ("staph_precise108", "StaphPRECISE108"),
+            ("staph_precise165", "StaphPRECISE165"),
+        ])
+    },
+    "Streptococcus_pyogenes": {
+        "GCF_001021955.1": ("s_pyogenes",
+        [
+            ("modulome", "Modulome"),
+        ])
+    },
+    "Streptomyces_albidoflavus": {
+        "GCF_000359525.1": ("s_albidoflavus",
+        [
+            ("Modulome", "Modulome"),
+        ])
+    },
+    "Synechococcus_elongatus": {
+        "GCF_000012525.1": ("s_elongatus",
+        [
+            ("elprecise300", "S. elongatuselPRECISE-300"),
+        ])
+    },
     "Vibrio_natriegens": {
         "GCF_001456255.1": ("v_natriegens", 
         [
@@ -15,10 +89,4 @@ IMODULON_DATA = {
             ("precise148", "natPRECISE148"),
         ])
     },
-    "Limosilactobacillus_reuteri": {
-        "GCF_020785475.1": ("l_reuteri",
-        [
-            ("lactoprecise", "L. reuteri lactoPRECISE"),
-        ])
-    }
 }
